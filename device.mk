fddfa0960dcfc0bda2a518252964eb607e0cc0ca
Edwin Wong <edwinwong@google.com>
2022-10-01 15:49:02 +0100

    a32: Build clearkey HIDL service from source

--- device.mk
+++ device.mk
@@ -86,6 +86,12 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
+    android.hardware.drm@1.4-service.clearkey
+
+PRODUCT_PACKAGES += \
+    libmockdrmcryptoplugin
+
+PRODUCT_PACKAGES += \
     android.hardware.drm@1.0.vendor \
     android.hardware.drm@1.1.vendor \
     android.hardware.drm@1.2.vendor \
