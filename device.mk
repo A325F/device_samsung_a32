4863603b654ac8e60830c7b7e1d5a5b25611febf
Bowgo Tsai <bowgotsai@google.com>
2022-10-01 15:54:47 +0100

    a32: Inherit developer GSI keys
    Bug: 144399552
    Test: tree-hugger

--- device.mk
+++ device.mk
@@ -7,6 +7,9 @@
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio-impl
