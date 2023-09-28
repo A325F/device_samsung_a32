a48178f7f02f681a60b39dcccd1ce038c34113af
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-09-28 22:31:06 +0300

    a32: Enable full VNDK

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -110,5 +110,8 @@ BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
 # VINTF
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
+# VNDK
+BOARD_VNDK_VERSION := current
+
 # Inherit the proprietary files
 include vendor/samsung/a32/BoardConfigVendor.mk
