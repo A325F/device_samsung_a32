92be4e47c8bed4050e5d4b506f0c4088b9c4a36d
AndroidHQ254 <d.murimi14@gmail.com>
2023-09-28 23:07:31 +0300

    a32: Build required GNSS interfaces
    Co-Authored-By: Ben <74118785+bengris32@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -60,6 +60,17 @@ PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
     android.hardware.gatekeeper@1.0-service
 
+# GNSS
+PRODUCT_PACKAGES += \
+    android.hardware.gnss@1.0.vendor \
+    android.hardware.gnss@1.1.vendor \
+    android.hardware.gnss@2.0.vendor \
+    android.hardware.gnss@2.1-impl \
+    android.hardware.gnss@2.1.vendor \
+    android.hardware.gnss.measurement_corrections@1.0.vendor \
+    android.hardware.gnss.measurement_corrections@1.1.vendor \
+    android.hardware.gnss.visibility_control@1.0.vendor
+
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
