2c55c87ea162c2d37732d56f8304a3da69e9db47
AndroidHQ254 <d.murimi14@gmail.com>
2023-09-28 23:01:59 +0300

    a32: Build Gatekeeper HIDL from source

--- device.mk
+++ device.mk
@@ -55,6 +55,11 @@ PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.1-impl-mock \
     fastbootd
 
+# Gatekeeper
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0-impl \
+    android.hardware.gatekeeper@1.0-service
+
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
