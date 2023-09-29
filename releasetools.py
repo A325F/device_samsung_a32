504463a1644c28d866302c85af9f9413d6e4aeb5
bengris32 <bengris32@protonmail.ch>
2023-09-29 06:38:07 +0300

    a32: Also build vbmeta images for system

--- releasetools.py
+++ releasetools.py
@@ -34,5 +34,6 @@ def AddImage(info, basename, dest):
 def OTA_InstallEnd(info):
   info.script.Print("Patching firmware images...")
   AddImage(info, "vbmeta.img", "/dev/block/by-name/vbmeta")
+  AddImage(info, "vbmeta_system.img", "/dev/block/by-name/vbmeta_system")
   AddImage(info, "dtbo.img", "/dev/block/by-name/dtbo")
   return
