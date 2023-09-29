29a7595038b96326bc4aea5ef217a1ddeb61c375
Bowgo Tsai <bowgotsai@google.com>
2023-09-29 06:51:11 +0300

    a32: Remove GSI keys
    Those keys will be embedded into VtsSecurityAvb on host side
    instead, to verify the GSI image used on the device.
    
    Bug: 149806769
    Test: build and checks those keys are removed from
          /recovery/root/first_stage_ramdisk/avb/

--- device.mk
+++ device.mk
@@ -7,9 +7,6 @@
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
-# Include GSI keys
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
-
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio-impl
