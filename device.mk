03921c47ce40d2095ada0cf50551d96e10b5f97c
bengris32 <bengris32@protonmail.ch>
2023-09-29 04:40:09 +0300

    a32: Build Thermal 2.0 interface

--- device.mk
+++ device.mk
@@ -299,6 +299,9 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@2.0-impl
 
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0.vendor
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
