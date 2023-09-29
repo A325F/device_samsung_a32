ff4a4154e2ba9a2494d1a6e36d7493f569f5b71f
Arne Coucheron <arco68@gmail.com>
2023-09-29 05:19:10 +0300

    a32: Disable serial console notification
    Google added a check for this prop in R, and if it's running it will show a notification about it that it's impacting performance.
    Why it's not disabled by default I don't know, so just force disable it here.

--- device.mk
+++ device.mk
@@ -277,7 +277,8 @@ PRODUCT_PACKAGES += \
     init.mt6769t.rc \
     init.recovery.mt6768.rc \
     init.recovery.mt6769t.rc \
-    init.recovery.samsung.rc
+    init.recovery.samsung.rc \
+    init.target.rc
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.mt6768:$(TARGET_COPY_OUT_RAMDISK)/fstab.mt6768 \
