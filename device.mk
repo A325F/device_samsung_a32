675a42b6ac3b0ac50c8961c3dcb486fc6f31bc9e
AndroidHQ254 <d.murimi14@gmail.com>
2023-09-29 04:44:11 +0300

    a32: Add `hardware/mediatek` to `PRODUCT_SOONG_NAMESPACES`

--- device.mk
+++ device.mk
@@ -294,7 +294,8 @@ PRODUCT_SHIPPING_API_LEVEL := 30
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    hardware/mediatek
 
 # Thermal
 PRODUCT_PACKAGES += \
