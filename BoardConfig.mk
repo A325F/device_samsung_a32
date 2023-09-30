567ee8989a40ebef164dca1047123dc930429ac5
AndroidHQ254 <d.murimi14@gmail.com>
2023-09-30 12:01:38 +0300

    a32: Generate `framework_compatibility_matrix.xml`
    * Add Lineage FCM as well while at it

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -143,6 +143,9 @@ TARGET_VIBRATOR_SUPPORTS_EFFECTS := true
 # VINTF
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/hidl/manifest.xml
 DEVICE_MATRIX_FILE += $(DEVICE_PATH)/configs/hidl/compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+	vendor/lineage/config/device_framework_matrix.xml \
+	$(DEVICE_PATH)/configs/hidl/framework_compatibility_matrix.xml
 
 # VNDK
 BOARD_VNDK_VERSION := current
