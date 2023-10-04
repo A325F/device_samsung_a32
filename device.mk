5cb54598aa294dee3c083b837740c88fed01b724
AndroidHQ254 <d.murimi14@gmail.com>
2023-10-04 20:30:49 +0300

    a32: Add missing USB packages
    F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/android.hardware.usb@1.3-service.coral": library "android.hardware.usb@1.1.so" not found: needed by main executable
    
    E HidlServiceManagement: Service android.hardware.usb@1.3::IUsb/default must be in VINTF manifest in order to register/get.

--- device.mk
+++ device.mk
@@ -399,7 +399,10 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
+    android.hardware.usb@1.0.vendor \
+    android.hardware.usb@1.1.vendor \
+    android.hardware.usb@1.2.vendor \
+    android.hardware.usb@1.3.vendor
 
 # Wi-Fi
 PRODUCT_PACKAGES += \
