60f1f58ba0b97aad9332f6103da4fe22063d2633
AndroidHQ254 <d.murimi14@gmail.com>
2023-10-04 20:27:29 +0300

    a32: Build missing `android.hardware.media.c2` libs
    F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/android.hardware.media.c2@1.2-mediatek": library "android.hardware.media.c2@1.0.so" not found: needed by main executable

--- device.mk
+++ device.mk
@@ -183,6 +183,9 @@ PRODUCT_PACKAGES += \
 
 # Media
 PRODUCT_PACKAGES += \
+    android.hardware.media.c2@1.0.vendor \
+    android.hardware.media.c2@1.1.vendor \
+    android.hardware.media.c2@1.2.vendor \
     android.hardware.media.omx@1.0-service
 
 # NFC
