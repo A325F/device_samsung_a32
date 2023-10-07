515d50b6216111680c42c796999b1dd68a4f34c5
AndroidHQ254 <d.murimi14@gmail.com>
2023-10-07 14:17:40 +0300

    a32: Build fingerprint HAL service
    10-07 04:12:36.693  1087  1153 D Fingerprint21: Daemon was null, reconnecting, current operation: null
    10-07 04:12:36.695  1087  1153 W Fingerprint21: NoSuchElementException
    10-07 04:12:36.695  1087  1153 W Fingerprint21: java.util.NoSuchElementException
    10-07 04:12:36.695  1087  1153 W Fingerprint21: 	at android.os.HwBinder.getService(Native Method)
    10-07 04:12:36.695  1087  1153 W Fingerprint21: 	at android.os.HwBinder.getService(HwBinder.java:81)
    10-07 04:12:36.695  1087  1153 W Fingerprint21: 	at android.hardware.biometrics.fingerprint.V2_1.IBiometricsFingerprint.getService(IBiometricsFingerprint.java:74)
    10-07 04:12:36.695  1087  1153 W Fingerprint21: 	at android.hardware.biometrics.fingerprint.V2_1.IBiometricsFingerprint.getService(IBiometricsFingerprint.java:84)
    10-07 04:12:36.695  1087  1153 W Fingerprint21: 	at com.android.server.biometrics.sensors.fingerprint.hidl.Fingerprint21.getDaemon(Fingerprint21.java:422)
    10-07 04:12:36.695  1087  1153 W Fingerprint21: 	at com.android.server.biometrics.sensors.fingerprint.hidl.Fingerprint21.isHardwareDetected(Fingerprint21.java:775)
    10-07 04:12:36.695  1087  1153 W Fingerprint21: 	at com.android.server.biometrics.sensors.fingerprint.FingerprintService$FingerprintServiceWrapper.isHardwareDetectedDeprecated(FingerprintService.java:683)
    10-07 04:12:36.695  1087  1153 W Fingerprint21: 	at android.hardware.fingerprint.IFingerprintService$Stub.onTransact(IFingerprintService.java:689)
    10-07 04:12:36.695  1087  1153 W Fingerprint21: 	at android.os.Binder.execTransactInternal(Binder.java:1285)
    10-07 04:12:36.695  1087  1153 W Fingerprint21: 	at android.os.Binder.execTransact(Binder.java:1244)
    10-07 04:12:36.695  1087  1153 W Fingerprint21: Fingerprint HAL not available

--- device.mk
+++ device.mk
@@ -173,6 +173,10 @@ PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.1-impl-mock \
     fastbootd
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service
+
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
