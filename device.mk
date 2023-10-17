19849ea5e330f291f5ef6e7fe465995e2c6eb898
AndroidHQ254 <d.murimi14@gmail.com>
2023-10-17 22:26:10 +0300

    a32: rro_overlays: Refactor device overlays

--- device.mk
+++ device.mk
@@ -299,6 +299,7 @@ PRODUCT_PACKAGES += \
     CarrierConfigOverlayA32 \
     FrameworksResOverlayA32 \
     SettingsOverlayA32 \
+    SystemUIOverlayA32 \
     TetheringConfigOverlayA32 \
     WifiOverlayA32
 
