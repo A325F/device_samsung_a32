411b813cf704d83cfbec12ff80e599cd0005cca4
AndroidHQ254 <d.murimi14@gmail.com>
2023-11-25 21:34:59 +0300

    Revert "a32: Inherit several Android Go configurations"
    This reverts commit d57fe6d1d9379e543784da6f74dc88da93d79143.

--- device.mk
+++ device.mk
@@ -101,19 +101,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libavservices_minijail_vendor
 
-# Dex
-PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
-PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
-
-# Dex - Debug
-ART_BUILD_TARGET_NDEBUG := true
-ART_BUILD_TARGET_DEBUG := false
-ART_BUILD_HOST_NDEBUG := true
-ART_BUILD_HOST_DEBUG := false
-PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
-PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
-USE_DEX2OAT_DEBUG := false
-
 # Disable SF configstore
 PRODUCT_PACKAGES += \
     disable_configstore
