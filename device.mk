9946c5943d6b6006628371db994b9df70b8569cf
AndroidHQ254 <d.murimi14@gmail.com>
2023-12-29 02:29:29 +0300

    a32: Build needed GNSS packages

--- device.mk
+++ device.mk
@@ -56,6 +56,17 @@ PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
     android.hardware.gatekeeper@1.0-service
 
+# GNSS
+PRODUCT_PACKAGES += \
+    android.frameworks.sensorservice@1.0.vendor \
+    android.hardware.gnss.measurement_corrections@1.0.vendor \
+    android.hardware.gnss.measurement_corrections@1.1.vendor \
+    android.hardware.gnss.visibility_control@1.0.vendor \
+    android.hardware.gnss@1.0.vendor \
+    android.hardware.gnss@1.1.vendor \
+    android.hardware.gnss@2.0.vendor \
+    android.hardware.gnss@2.1.vendor
+
 # Graphics
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@4.0.vendor \
