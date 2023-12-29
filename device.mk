37b4f0ef155f637bfc33a5019ad76fa0d293a25e
AndroidHQ254 <d.murimi14@gmail.com>
2023-12-29 03:07:42 +0300

    a32: Build & add needed Power packages

--- device.mk
+++ device.mk
@@ -125,6 +125,10 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Power
+PRODUCT_PACKAGES += \
+    android.hardware.power-V2-ndk_platform.vendor
+
 # Product characteristics
 PRODUCT_CHARACTERISTICS := default
 
